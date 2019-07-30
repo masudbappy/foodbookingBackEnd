5e8c17fa462f5bc31d5bfb3bf0cdf865055a5091
masudbappy <masud.bappy90@gmail.com>
2019-07-30 14:46:48 +0600

    Create Project Structure

---
+++ src/main/java/com/masudbappy/foodbooking/FoodbookingApplication.java
@@ -0,0 +1,13 @@
+package com.masudbappy.foodbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodbookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodbookingApplication.class, args);
+    }
+
+}
